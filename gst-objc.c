a80b4e0b852512cb24c8377a70110e3e1cd32a2f
Mathieu Suen <mathk.sue@gmail.com>
2011-02-14 23:55:55 +0100

    Work on load nib/xib file and NSString

--- gst-objc.c
+++ gst-objc.c
@@ -4,11 +4,20 @@
 
 //id objc_msg_Send (id, SEL, ...);
 
+int
+gst_NSApplicationMain()
+{
+  char args[] =  {"gst-cocoa"};
+  char **argv = &args;
+  return NSApplicationMain(1, argv);
+}
+
 void
 gst_initModule (VMProxy * proxy)
 {
-  proxy->dlOpen("libobjc", false);
-  proxy->dlOpen("Cocoa.framework/Cocoa", false);
+  proxy->dlOpen ("libobjc", false);
+  proxy->dlOpen ("Cocoa.framework/Cocoa", false);
+  proxy->defineCFunc ("NSApplicationMain", gst_NSApplicationMain);
 
 }
 
