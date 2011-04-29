dbf9f42295bdcd3951e7b5816e04573303c8a257
Mathieu Suen <mathk.sue@gmail.com>
2011-04-29 23:48:35 +0200

    Missing file

---
+++ gst-objc.h
@@ -0,0 +1,9 @@
+#include <gstpub.h>
+#import "gst-objc-ext.h"
+#import <Foundation/Foundation.h>
+//#import "LKInterpreterRuntime.h"
+#ifdef __APPLE__
+#include <objc/runtime.h>
+#endif
+
+extern VMProxy* gst_proxy;
